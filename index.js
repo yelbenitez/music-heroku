e4fb4ea21318fbbfc4b6940007902b4bc8ff1a59
Yel <yelbenitez5@gmail.com>
2018-12-10 11:38:17 -0800

    test response

--- index.js
+++ index.js
@@ -52,47 +52,7 @@ try{
 
                             response.setHeader('Content-Type', 'application/json', 'charset=utf-16');
                             var pass = {
-                                "expectUserResponse": true,
-                                "expectedInputs": [
-                                    {
-                                        "inputPrompt": {
-                                            "richInitialPrompt": {
-                                                "items": [
-                                                    {
-                                                        "simpleResponse": {
-                                                            "textToSpeech": "Math and prime numbers it is!"
-                                                        }
-                                                    },
-                                                    {
-                                                        "basicCard": {
-                                                            "title": "Math & prime numbers",
-                                                            "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
-                                                            "image": {
-                                                                "url": "https://example.google.com/42.png",
-                                                                "accessibilityText": "Image alternate text"
-                                                            },
-                                                            "buttons": [
-                                                                {
-                                                                    "title": "Read more",
-                                                                    "openUrlAction": {
-                                                                        "url": "https://example.google.com/mathandprimes"
-                                                                    }
-                                                                }
-                                                            ],
-                                                            "imageDisplayOptions": "CROPPED"
-                                                        }
-                                                    }
-                                                ],
-                                                "suggestions": []
-                                            }
-                                        },
-                                        "possibleIntents": [
-                                            {
-                                                "intent": "actions.intent.TEXT"
-                                            }
-                                        ]
-                                    }
-                                ]
+                                    "fulfillmentText": "playing..."
                             }
                                
                             response.send(pass); 
