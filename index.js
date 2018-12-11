b2cffa98d76d1539f226461b0ceef752fc67c952
Yel <yelbenitez5@gmail.com>
2018-12-10 18:50:55 -0800

    widget player

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ try{
                //     var location = bodyContent.queryResult.parameters["any"];
 
                     req.header({'Content-Type':'application/json'});
-                    req.header({'Authorization':'Bearer BQBNNwy2dC_GXrsQeNMI7A1MdGVnppYaivawao0PHxUovWQJY9CHmK_7Q1WHC1amwuW_pvbVaWBp20ktRljQftJL0OOhdZHh9gJVF_TsUXotMt2CiVotAnPTsOHQ40fFlYf9TuSbIvu7lNrgHPc4k1MibFkhTry7pA'})
+                    req.header({'Authorization':'Bearer BQCWZhUN2zR7ri20H8W9MD-z6J5mdoFjbGKehmceFJ9mPyLEqO7w0_WeVcE-4EuO7yVkyqmr1J4uW7UhmNfpn9SLeC7wa1olzWIfGR0TuYD_lRP7_xYjXbTAmswWdEptjACJ15n0lXxd4jg3_KaX8Xlv_cMg3d4omA'})
                     req.query({
                         "q": "name:"+song,
                         "type": "album,track"
@@ -55,10 +55,48 @@ try{
 
                             let track = res.body.tracks.items[0];
                             let externalLink = track.album.artists[0].external_urls["spotify"];
+                            let contentUrl = track["preview_url"]; // only preview
+                            let icon = track.album.images[0]["url"];
 
                             response.setHeader('Content-Type', 'application/json', 'charset=utf-16');
-                            
-                           
+                            var pass = {
+                                "payload": {
+                                    "google": {
+                                      "expectUserResponse": true,
+                                      "richResponse": {
+                                        "items": [
+                                          {
+                                            "simpleResponse": {
+                                              "textToSpeech": "Now playing : "
+                                            }
+                                          },
+                                          {
+                                            "mediaResponse": {
+                                              "mediaType": "AUDIO",
+                                              "mediaObjects": [
+                                                {
+                                                  "contentUrl": contentUrl,
+                                                  "description": song,
+                                                  "icon": {
+                                                    "url": icon,
+                                                    "accessibilityText": "Ocean view"
+                                                  },
+                                                  "name": "Jazz in Paris"
+                                                }
+                                              ]
+                                            }
+                                          }
+                                        ],
+                                        "suggestions": [
+                                          {
+                                            "title": "Cancel"
+                                          }
+                                        ]
+                                      }
+                                    }
+                                  }
+                            }
+                        /*   
                             var pass = {
                                 
                                 "payload": {
@@ -105,6 +143,8 @@ try{
                                   ]
                             }
    
+*/
+
                             response.send(pass); 
                         }
                     });
