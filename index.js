00268fdc33c0f76e1d6f14a56e7b1dd6f86b2d5b
Yel <yelbenitez5@gmail.com>
2018-12-10 16:28:54 -0800

    nothing

--- index.js
+++ index.js
@@ -99,10 +99,7 @@ try{
                                     {
                                       "text": {
                                         "text": [
-                                          "[I'm an inline-style link](https://www.google.com)"
-                                        ],
-                                        "url":[
-                                            "https://firebasestorage.googleapis.com/v0/b/music-6643e.appspot.com/o/512.png?alt=media&token=9c463039-24c6-4947-8dcf-5a25d0628856"
+                                          externalLink
                                         ]
                                       }
                                     }
